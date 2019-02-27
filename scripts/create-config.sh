76b132287766a5e809c813777855b9074a1ad7b1
Rhys Evans <rhys301097@gmail.com>
2019-02-27 19:40:45 +0000

    moved default config creation to scripts/

---
+++ scripts/create-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Create default config file for travis and tests
+
+echo '{"jwtSecret": "defaultsecret", "jwtExpiry": 300, "mongoURI": "mongodb://mongo:27017/taxe-server"}' >> ../config.json
